66f352a9f5a25b2a2a5444289037bc301adc797d
lugaxker <lug.axker@gmail.com>
2018-03-16 23:37:34 +0000

    fixing transaction stuff (BIP143)

--- broadcast_transaction.py
+++ broadcast_transaction.py
@@ -8,6 +8,7 @@ from crypto import (double_sha256, EllipticCurveKey)
 
 SIGHASH_ALL = 0x01
 SIGHASH_FORKID = 0x40
+FORKID = 0x00
 BCH_SIGHASH_TYPE = 0x41
 
 TRANSACTION_VERSION_1 = 1 # version 2 transactions exist
@@ -19,13 +20,13 @@ OP_HASH160= 0xa9
 OP_EQUALVERIFY = 0x88
 OP_CHECKSIG = 0xac
 
-def construct_transaction( wifkey, receive_address, amount, locktime ):
-    ''' Construct a Bitcoin Cash transaction.
+def construct_transaction( wifkey, receive_address, amount, locktime, prevtx_id, prevtx_index ):
+    ''' Construct a Bitcoin Cash transaction with one input and one output.
     wifkey (str) : private key (Wallet Import Format)
     receive_address (str) : recipient address (legacy or cash format)
-    amount (int) : amount in satoshis '''
-    
-    
+    amount (int) : amount in satoshis 
+    prevtx_id (str) : previous output transaction id
+    prevtx_index (int) : index of the output in the previous transaction'''
     
     # Creation of elliptic curve keys (private key + public key)
     eckey = EllipticCurveKey.from_wifkey( wifkey )
@@ -53,17 +54,19 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     nSequence = sequence.to_bytes(4,'little')
     
     # Amount in satoshis
-    nAmount = amount.to_bytes(4,'little')
+    nAmount = amount.to_bytes(8,'little')
     
     # Locktime
     nLocktime = locktime.to_bytes(4,'little')
     
     # Previous output hash (previous transaction id)
-    prevoutHash = bytes.fromhex("bda694b9278473b080c97a37c5210aa7c87f80417d229ab5efc03f9c9909f5f5")
+    prevHash = bytes.fromhex( prevtx_id )[::-1]
     
     # Previous output index in this transaction
-    index = 1 # second output
-    prevoutIndex = index.to_bytes(4,'little')
+    prevIndex = prevtx_index.to_bytes(4,'little')
+    
+    # Previous output value
+    prevValue = prevamount.to_bytes(8,'little')
     
     # Number of tx inputs
     input_count = 1
@@ -74,7 +77,7 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     nOutputs = bytes([output_count])
     
     # Input Public Key Hash
-    pubkeyHash_in = sendaddr.hash_addr[::-1]
+    pubkeyHash_in = sendaddr.hash_addr
     
     # Previous locking script
     prevLockingScript = bytes([OP_DUP]) + bytes([OP_HASH160]) + bytes([0x14]) + pubkeyHash_in + bytes([OP_EQUALVERIFY]) + bytes([OP_CHECKSIG])
@@ -83,7 +86,7 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     lengthPrevLockingScript = bytes([len(prevLockingScript)])
     
     # Output Public Key Hash
-    pubkeyHash_out = sendaddr.hash_addr[::-1]
+    pubkeyHash_out = recaddr.hash_addr
     
     # Locking script
     lockingScript = bytes([OP_DUP]) + bytes([OP_HASH160]) + bytes([0x14]) + pubkeyHash_out + bytes([OP_EQUALVERIFY]) + bytes([OP_CHECKSIG])
@@ -91,39 +94,41 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     # Length of previous locking script
     lengthLockingScript = bytes([len(lockingScript)])
     
+    
+    outpoint = prevHash + prevIndex
+    hashPrevouts = double_sha256( outpoint )
+    hashSequence = double_sha256( nSequence )
+    hashOutputs = double_sha256( nAmount + lengthLockingScript + lockingScript )
+    
     # --- Construct preimage (temporary transaction) ---
+    # BIP-143
     preimage = b""
     
     preimage += nVersion # version
-    preimage += nInputs # input count
-    
-    preimage += prevoutHash # previous output hash
-    preimage += prevoutIndex # previous output index
+    preimage += hashPrevouts
+    preimage += hashSequence
+    preimage += outpoint
     preimage += lengthPrevLockingScript # length of the previous output locking script
     preimage += prevLockingScript # previous output locking script (scriptPubKey)
+    preimage += prevValue # value of the previous output
     preimage += nSequence # sequence number
-    
-    preimage += nOutputs # output count
-    
-    preimage += nAmount # value of the output
-    preimage += lengthLockingScript # length of the output locking script
-    preimage += lockingScript # output locking script
-    
+    preimage += hashOutputs
     preimage += nLocktime # locktime
     preimage += nHashtype # signature 4-bytes hash type
     
-    print("1. Preimage\n", preimage.hex())
-    print()
+    #print("Preimage")
+    #print(preimage.hex())
+    #print()
     
     # We sign the double SHA256 hash of the preimage with our private key
-    preimage_hash = double_sha256( preimage )
-    signature = eckey.sign( preimage_hash )
-    lengthSig = bytes([len(signature)])
+    prehash = double_sha256( preimage )
+    signature = eckey.sign( prehash )
+    lengthSigandHash = bytes([len(signature)+1])
     
     # --- Construct unlocking script ---
     unlockingScript = b""
     
-    unlockingScript += lengthSig # length of the signature
+    unlockingScript += lengthSigandHash # length of the signature
     unlockingScript += signature # DER-encoded signature of the double sha256 of the preimage
     unlockingScript += bytes([hashtype]) # signature 1-byte hash type
     unlockingScript += lengthPublicKey # length of serialized public key
@@ -131,8 +136,9 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     
     lengthUnlockingScript = bytes([len(unlockingScript)])
     
-    print("2. Unlocking script\n", unlockingScript.hex())
-    print()
+    #print("Unlocking script")
+    #print(unlockingScript.hex())
+    #print()
     
     # --- Construct transaction ---
     rawtx = b""
@@ -140,8 +146,8 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     rawtx += nVersion # version
     rawtx += nInputs # input count
     
-    rawtx += prevoutHash # previous output hash
-    rawtx += prevoutIndex # previous output index
+    rawtx += prevHash # previous output hash
+    rawtx += prevIndex # previous output index
     rawtx += lengthUnlockingScript # length of the previous output locking script
     rawtx += unlockingScript # previous output locking script (scriptPubKey)
     rawtx += nSequence # sequence number
@@ -154,10 +160,16 @@ def construct_transaction( wifkey, receive_address, amount, locktime ):
     
     rawtx += nLocktime # locktime
     
-    print("3. Transaction\n", rawtx.hex())
-    print()
+    txid = double_sha256( rawtx )[::-1]
     
-    return rawtx.hex()
+    #print("Raw transaction")
+    #print(rawtx.hex())
+    #print()
+    #print("Transaction ID")
+    #print(txid.hex())
+    #print()
+    
+    return rawtx.hex(), txid.hex()
     
     
 if __name__ == '__main__':
@@ -166,12 +178,18 @@ if __name__ == '__main__':
     print("---------------------")
     print()
     
-    wifkey = "5KLnc4W67hQ2NYPky89WJYmEGfh42ddkui9YL7px9EnJq3KA6KE"
+    wifkey = "5KdpawjKDYiMAS9fLtXnTgsmtynT3BrRz5xsXqwBZ5s8grqUh9X"
     recipient_address = "bitcoincash:qq7ur36zd8uq2wqv0mle2khzwt79ue9ty57mvd95r0"
-    amount = 50000
-    locktime = 0
-    tx = construct_transaction( wifkey, recipient_address, amount, locktime )
-    print("Raw transaction")
-    print(tx)
+    amount = 24700
+    locktime = 521730
+    prevtx_id = "be95987a30f78575fa19baa1a48162f00d1b2f03af6ed7168cdacbbd7a6875f4"
+    prevtx_index = 1
+    prevamount = 25000
     
+    tx, txid = construct_transaction( wifkey, recipient_address, amount, locktime, prevtx_id, prevtx_index )
+    print("RAW TRANSACTION")
+    print(tx)
+    print()
+    print("TRANSACTION ID")
+    print(txid)
     print()
