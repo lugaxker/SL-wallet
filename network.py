e8937a622c71f1d0534f0847e18989cd20db6fde
lugaxker <lug.axker@gmail.com>
2018-04-02 11:12:03 +0000

    connexion to a bitcoin node (version message)

--- network.py
+++ network.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import time
+import random
+import socket
 
 from crypto import dsha256
 
@@ -11,58 +13,79 @@ START_STRING = 0xd9b4bef9
 
 PROTOCOL_VERSION = 70015
 SPV_SERVICES = 0
-ZERO_NONCE = 0
-NO_RELAY = 0
+RELAY = False
 
 localhost_ipv6_address = 0x00000000000000000000ffff7f000001 #127.0.0.1
 
-# --- Version message ---
+def make_message( cmd, payload ):
+    ''' Message Structure for Bitcoin Protocol.
+    cmd (str) : command (e.g. "version")
+    payload (bytes) : actual data '''
+    
+    magic = START_STRING.to_bytes(4,'little')
+    cmdb = cmd.encode('ascii')
+    command = cmdb + ( ( 12 - len(cmdb) ) * b"\00" )
+    length = len(payload).to_bytes(4, 'little')
+    checksum = dsha256( payload )[:4]
+    return magic + command + length + checksum + payload
 
-# Protocol Version
-version = PROTOCOL_VERSION.to_bytes(4, 'little')
+def version_message(last_block):
+    ''' Version message. '''
+    
+    # Protocol Version
+    version = PROTOCOL_VERSION.to_bytes(4, 'little')
 
-# Services
-services = SPV_SERVICES.to_bytes(8, 'little')
+    # Services
+    services = SPV_SERVICES.to_bytes(8, 'little')
+    
+    # Timestamp
+    t = int( time.time() )
+    timestamp = t.to_bytes(8, 'little')
+    
+    # Receiving address
+    addr_recv  = SPV_SERVICES.to_bytes(8, 'little')
+    addr_recv += localhost_ipv6_address.to_bytes(16,'big')
+    addr_recv += DEFAULT_PORT.to_bytes(2,'big')
+    
+    # Transmitting address
+    addr_trans  = SPV_SERVICES.to_bytes(8, 'little')
+    addr_trans += localhost_ipv6_address.to_bytes(16,'big')
+    addr_trans += DEFAULT_PORT.to_bytes(2,'big')
+    
+    # Nonce
+    nonce = random.getrandbits(64).to_bytes(8, 'little')
+    
+    user_agent_bytes = bytes([0x00])
+    start_height = last_block.to_bytes(4, 'little')
+    relay = bytes([RELAY])
+    
+    payload = version + services + timestamp + addr_recv + addr_trans + nonce + user_agent_bytes + start_height + relay
+    
+    return payload
+    
 
-# Timestamp
-t = int( time.time() )
-timestamp = t.to_bytes(8, 'little')
+if __name__ == '__main__':
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    host = "195.154.227.47"
+    port = DEFAULT_PORT
+    print("connecting to node...")
+    sock.connect((host,port))
+    
+    last_block = 516269 # !!!
+    payload = version_message(last_block)
+    message = make_message("version", payload)
+    print("Version message", message.hex())
+    sock.send( message )
+    
+    m = sock.recv( 1024 )
+    print("version", m.hex())
+    
+    m = sock.recv( 1024 )
+    print("verack", m.hex())
 
-# Receiving address
-addr_recv_services = SPV_SERVICES.to_bytes(8, 'little')
-addr_recv_ip = localhost_ipv6_address.to_bytes(16,'big')
-addr_recv_port = DEFAULT_PORT.to_bytes(2,'big')
-addr_recv = addr_recv_services + addr_recv_ip + addr_recv_port
-
-# Transmitting address
-addr_trans_services = SPV_SERVICES.to_bytes(8, 'little')
-addr_trans_ip = localhost_ipv6_address.to_bytes(16,'big')
-addr_trans_port = DEFAULT_PORT.to_bytes(2,'big')
-addr_trans = addr_trans_services + addr_trans_ip + addr_trans_port
-
-# Nonce
-nonce = ZERO_NONCE.to_bytes(8, 'little')
-
-user_agent_bytes = bytes([0x00])
-
-last_block = 524030 # !!!
-start_height = last_block.to_bytes(4, 'little')
-
-relay = bytes([NO_RELAY])
-
-payload = version + services + timestamp + addr_recv + addr_trans + nonce + user_agent_bytes + start_height + relay
-payload_len = bytes([len(payload)])
-checksum = dsha256( payload )[:4]
-
-magic = START_STRING.to_bytes(4,'little')
-
-msg = "version".encode('ascii')
-command = msgb + ( ( 12 - len(msg) ) * b"\00" )
-
-message = magic + command + payload_len + checksum + payload
-
-print("Version message", message.hex())
-
-
+    sock.close()
+    
 
 
