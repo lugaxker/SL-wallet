7af02b599e2a56d3c1ff1ba33da142e41849af41
lugaxker <lug.axker@gmail.com>
2018-08-10 17:47:39 +0000

    removed prints

--- blockexplorer.py
+++ blockexplorer.py
@@ -8,7 +8,6 @@ from address import Address
 
 def get_address_utxos( address ):
     ''' address: Address '''
-    print("get_address_utxos")
     addr = address.to_cash()
     
     confirmed_outputs_url = "https://api.blockchair.com/bitcoin-cash/outputs"
@@ -26,17 +25,4 @@ def get_address_utxos( address ):
             utxos.append( {'txid': o['transaction_hash'], 'index': o['index'], 'value': int(o['value']), 'address': address})
     
     return utxos
-    
-if __name__ == '__main__':
-    
-    import sys
-    if sys.version_info < (3, 5):
-        sys.exit("Error: Must be using Python 3.5 or higher")
-    
-    addr = Address.from_string( "qq7ur36zd8uq2wqv0mle2khzwt79ue9ty57mvd95r0" )
-    utxos = get_address_utxos( addr )
-    for o in utxos:
-        print(o)
-    value = sum( o['value'] for o in utxos )
-    print( "value: {:.8f} BCH".format( value/1e8 ) )
     
